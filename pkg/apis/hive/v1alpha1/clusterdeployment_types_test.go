c5f79497296a9e7284d62155dc342e29e0ff737a
Devan Goodwin <dgoodwin@redhat.com>
2018-09-13 08:24:42 -0300

    Add initial ClusterDeployment based on InstallConfig.

--- pkg/apis/hive/v1alpha1/clusterdeployment_types_test.go
+++ pkg/apis/hive/v1alpha1/clusterdeployment_types_test.go
@@ -27,7 +27,12 @@ import (
 
 func TestStorageClusterDeployment(t *testing.T) {
 	key := types.NamespacedName{Name: "foo", Namespace: "default"}
-	created := &ClusterDeployment{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"}}
+	created := &ClusterDeployment{
+		ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"},
+		Spec: ClusterDeploymentSpec{
+			Machines: []MachinePool{},
+		},
+	}
 	g := gomega.NewGomegaWithT(t)
 
 	// Test Create

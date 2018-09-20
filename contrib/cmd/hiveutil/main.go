ca98a4fd079b124bf2d14dc262c09a6ba1afb2f8
Joel Diaz <jdiaz@redhat.com>
2018-09-20 10:36:01 -0400

    add initial hiveutil
    first coutil -> hiveutil migration (right now only support aws-tag-deprovision subcommand)

---
+++ contrib/cmd/hiveutil/main.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/openshift/hive/contrib/pkg/aws_tag_deprovision"
+)
+
+func main() {
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+
+	cmd := NewCOUtilityCommand()
+
+	err := cmd.Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error occurred: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func NewCOUtilityCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "hiveutil SUB-COMMAND",
+		Short: "Utilities for hive",
+		Long:  "Contains various utilities for running and testing hive",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Usage()
+		},
+	}
+	cmd.AddCommand(aws_tag_deprovision.NewDeprovisionAWSWithTagsCommand())
+
+	return cmd
+}

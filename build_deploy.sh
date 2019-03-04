b226f80d2eec7dec18759275400c2b01e5d3fbd6
Jaime Melis <jmelis@redhat.com>
2019-03-04 17:05:53 +0100

    build_deploy.sh and pr_check.sh for AppSRE CI/CD
    This adds the required scripts for AppSRE CI/CD
    
    - `build_deploy.sh` will be executed in merge to master. It currently
      builds and pushes to quay.
     - `pr_check.sh` currently doesn't do anything as the CI is done via
       openshift-ci.

---
+++ build_deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# AppSRE team CD
+
+set -exv
+
+BASE_IMG="hive-controller"
+IMG="${BASE_IMG}:latest"
+
+BUILD_CMD="docker build" IMG="$IMG" make docker-build
+
+DEST_IMG="quay.io/app-sre/${BASE_IMG}"
+
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${IMG}" \
+    "docker://${DEST_IMG}:latest"
+
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${IMG}" \
+    "docker://${DEST_IMG}:$(git rev-parse --short=7 HEAD)"

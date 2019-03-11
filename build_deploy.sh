74be230443ead92f463c8d363e1a1f65cee37662
Jaime Melis <jmelis@redhat.com>
2019-03-11 22:22:27 +0100

    Create operator registry container in AppSRE CI/CD
    The build_deploy.sh script now creates and pushes the hive-registry olm
    catalog container.
    
    It uses this repo to support keep the story of CSV versions and to
    generate the hive-registry image:
    
    https://github.com/app-sre/saas-hive-operator-bundle/tree/staging

--- build_deploy.sh
+++ build_deploy.sh
@@ -9,14 +9,13 @@ GIT_COMMIT_COUNT=`git rev-list 9c56c62c6d0180c27e1cc9cf195f4bbfd7a617dd..HEAD --
 
 BASE_IMG="hive"
 IMG="${BASE_IMG}:latest"
+
 QUAY_IMAGE="quay.io/app-sre/${BASE_IMG}"
 
-BUNDLE_DIR="bundle"
-
-# Build the image
+# build the image
 BUILD_CMD="docker build" IMG="$IMG" make docker-build
 
-# Push the image
+# push the image
 skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
     "docker-daemon:${IMG}" \
     "docker://${QUAY_IMAGE}:latest"
@@ -25,12 +24,12 @@ skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
     "docker-daemon:${IMG}" \
     "docker://${QUAY_IMAGE}:${GIT_HASH}"
 
-# Clone bundle repo
+# clone bundle repo
 SAAS_OPERATOR_DIR="saas-hive-operator-bundle"
 BUNDLE_DIR="$SAAS_OPERATOR_DIR/hive/"
 BRANCH_CHANNEL="staging"
 
-trap "rm -f $SAAS_OPERATOR_DIR" EXIT TERM INT
+# trap "rm -f $SAAS_OPERATOR_DIR" EXIT TERM INT
 rm -rf "$SAAS_OPERATOR_DIR"
 
 git clone \
@@ -39,25 +38,56 @@ git clone \
     $SAAS_OPERATOR_DIR
 
 # generate bundle
-LAST_BUNDLE=$(ls $BUNDLE_DIR | sort -t . -k 3 -g | tail -n 1)
+PREV_VERSION=$(ls $BUNDLE_DIR | sort -t . -k 3 -g | tail -n 1)
 
 ./hack/generate-operator-bundle.py \
     $BUNDLE_DIR \
-    $LAST_BUNDLE \
+    $PREV_VERSION \
     $GIT_COMMIT_COUNT \
     $GIT_HASH \
     $QUAY_IMAGE
 
-(
-    cd $SAAS_OPERATOR_DIR
+# create package yaml
+NEW_VERSION=$(ls $BUNDLE_DIR | sort -t . -k 3 -g | tail -n 1)
+cat <<EOF > $BUNDLE_DIR/hive.package.yaml
+packageName: hive-operator
+channels:
+- name: $BRANCH_CHANNEL
+  currentCSV: hive-operator.v${NEW_VERSION}
+EOF
 
-    # add, commit & push
-    git add .
+# add, commit & push
+pushd $SAAS_OPERATOR_DIR
 
-    MESSAGE=$'add version $GIT_COMMIT_COUNT-$GIT_HASH\n\nreplaces $LAST_BUNDLE'
-    git commit -m "$MESSAGE"
+git add .
 
-    git push origin "$BRANCH_CHANNEL"
-)
+MESSAGE=$'add version $GIT_COMMIT_COUNT-$GIT_HASH\n\nreplaces $PREV_VERSION'
+git commit -m "$MESSAGE"
 
-# TODO: create and push operator catalog container
+git push origin "$BRANCH_CHANNEL"
+
+popd
+
+# build the registry image
+REGISTRY_IMG="quay.io/app-sre/hive-registry:${BRANCH_CHANNEL}"
+DOCKERFILE_REGISTRY="Dockerfile.olm-registry"
+
+cat <<EOF > $DOCKERFILE_REGISTRY
+FROM quay.io/openshift/origin-operator-registry:latest
+
+COPY $SAAS_OPERATOR_DIR manifests
+RUN initializer
+
+CMD ["registry-server", "-t", "/tmp/terminate.log"]
+EOF
+
+docker build -f $DOCKERFILE_REGISTRY --tag "$REGISTRY_IMG" .
+
+# push image
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${REGISTRY_IMG}" \
+    "docker://${REGISTRY_IMG}"
+
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${REGISTRY_IMG}" \
+    "docker://${REGISTRY_IMG}-$GIT_HASH"

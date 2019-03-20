cf4acdcd6e4d58831cd35bbc53240286406185bb
Devan Goodwin <dgoodwin@redhat.com>
2019-03-20 11:52:31 -0300

    Add a custom imageset cluster deployment template.

--- hack/e2e-test.sh
+++ hack/e2e-test.sh
@@ -98,7 +98,7 @@ while [ $i -le ${max_tries} ]; do
   fi
 
   echo "Generating ClusterDeployment File ${CLUSTER_NAME}. Try #${i}/${max_tries}:"
-  if oc process -f config/templates/cluster-deployment.yaml \
+  if oc process -f config/templates/cluster-deployment-customimageset.yaml \
          CLUSTER_NAME="${CLUSTER_NAME}" \
          SSH_KEY="${SSH_PUB_KEY}" \
          PULL_SECRET="${PULL_SECRET}" \
@@ -107,7 +107,6 @@ while [ $i -le ${max_tries} ]; do
          BASE_DOMAIN="${BASE_DOMAIN}" \
          HIVE_IMAGE="${HIVE_IMAGE}" \
          RELEASE_IMAGE="${RELEASE_IMAGE}" \
-         CLUSTER_IMAGE_SET="custom" \
          TRY_INSTALL_ONCE="true" \
          TRY_UNINSTALL_ONCE="true" \
       > ${CLUSTER_DEPLOYMENT_FILE} ; then

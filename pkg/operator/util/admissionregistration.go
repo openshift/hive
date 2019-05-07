5ef1a1ad22d337c1e4de07d8af81e1adc2c10b89
Cesar Wong <cewong@redhat.com>
2019-05-07 13:56:23 -0400

    Cluster deprovision request - phase 2
    Adds spec.state field to ClusterDeployment
    Adds validation for the field in the validation webhook
    and defaulting for it in a new mutating webhook
    Switches the clusterdeployment controller to use
    ClusterDeprovisionRequests to delete clusterdeployments instead of
    itself generating a job.

--- pkg/operator/util/admissionregistration.go
+++ pkg/operator/util/admissionregistration.go
@@ -33,3 +33,15 @@ func ReadValidatingWebhookConfigurationV1Beta1OrDie(objBytes []byte, scheme *run
 	}
 	return requiredObj.(*admregv1.ValidatingWebhookConfiguration)
 }
+
+// ReadMutatingWebhookConfigurationV1Beta1OrDie reads a MutatingWebhookConfiguration,
+// as this is not yet added to library-go.
+func ReadMutatingWebhookConfigurationV1Beta1OrDie(objBytes []byte, scheme *runtime.Scheme) *admregv1.MutatingWebhookConfiguration {
+	apiExtensionsCodecs := serializer.NewCodecFactory(scheme)
+
+	requiredObj, err := runtime.Decode(apiExtensionsCodecs.UniversalDecoder(admregv1.SchemeGroupVersion), objBytes)
+	if err != nil {
+		panic(err)
+	}
+	return requiredObj.(*admregv1.MutatingWebhookConfiguration)
+}

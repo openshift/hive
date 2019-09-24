3d1f748d2a7241914887a353f73351e2437eb7a1
staebler <staebler@redhat.com>
2019-09-24 12:29:53 -0400

    vendor for gcp destroy

---
+++ vendor/github.com/openshift/installer/pkg/destroy/gcp/gcp.go
@@ -0,0 +1,273 @@
+package gcp
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/pborman/uuid"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/wait"
+
+	resourcemanager "google.golang.org/api/cloudresourcemanager/v1"
+	compute "google.golang.org/api/compute/v1"
+	dns "google.golang.org/api/dns/v1"
+	"google.golang.org/api/googleapi"
+	iam "google.golang.org/api/iam/v1"
+	"google.golang.org/api/option"
+	storage "google.golang.org/api/storage/v1"
+
+	gcpconfig "github.com/openshift/installer/pkg/asset/installconfig/gcp"
+	"github.com/openshift/installer/pkg/destroy/providers"
+	"github.com/openshift/installer/pkg/types"
+	gcptypes "github.com/openshift/installer/pkg/types/gcp"
+	"github.com/openshift/installer/pkg/version"
+)
+
+var (
+	defaultTimeout = 2 * time.Minute
+)
+
+// ClusterUninstaller holds the various options for the cluster we want to delete
+type ClusterUninstaller struct {
+	Logger    logrus.FieldLogger
+	Region    string
+	ProjectID string
+	ClusterID string
+	Context   context.Context
+
+	computeSvc *compute.Service
+	iamSvc     *iam.Service
+	dnsSvc     *dns.Service
+	storageSvc *storage.Service
+	rmSvc      *resourcemanager.Service
+
+	// cloudControllerUID is the cluster ID used by the cluster's cloud controller
+	// to generate load balancer related resources. It can be obtained either
+	// from metadata or by inferring it from existing cluster resources.
+	cloudControllerUID string
+
+	requestIDTracker
+	pendingItemTracker
+}
+
+// New returns an AWS destroyer from ClusterMetadata.
+func New(logger logrus.FieldLogger, metadata *types.ClusterMetadata) (providers.Destroyer, error) {
+	return &ClusterUninstaller{
+		Logger:             logger,
+		Region:             metadata.ClusterPlatformMetadata.GCP.Region,
+		ProjectID:          metadata.ClusterPlatformMetadata.GCP.ProjectID,
+		ClusterID:          metadata.InfraID,
+		Context:            context.Background(),
+		cloudControllerUID: gcptypes.CloudControllerUID(metadata.InfraID),
+		requestIDTracker:   newRequestIDTracker(),
+		pendingItemTracker: newPendingItemTracker(),
+	}, nil
+}
+
+// Run is the entrypoint to start the uninstall process
+func (o *ClusterUninstaller) Run() error {
+	ctx, cancel := o.contextWithTimeout()
+	defer cancel()
+
+	ssn, err := gcpconfig.GetSession(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get session")
+	}
+
+	options := []option.ClientOption{
+		option.WithCredentials(ssn.Credentials),
+		option.WithUserAgent(fmt.Sprintf("OpenShift/4.x Destroyer/%s", version.Raw)),
+	}
+
+	o.computeSvc, err = compute.NewService(ctx, options...)
+	if err != nil {
+		return errors.Wrap(err, "failed to create compute service")
+	}
+
+	o.iamSvc, err = iam.NewService(ctx, options...)
+	if err != nil {
+		return errors.Wrap(err, "failed to create compute service")
+	}
+
+	o.dnsSvc, err = dns.NewService(ctx, options...)
+	if err != nil {
+		return errors.Wrap(err, "failed to create dns service")
+	}
+
+	o.storageSvc, err = storage.NewService(ctx, options...)
+	if err != nil {
+		return errors.Wrap(err, "failed to create storage service")
+	}
+
+	o.rmSvc, err = resourcemanager.NewService(ctx, options...)
+	if err != nil {
+		return errors.Wrap(err, "failed to create resourcemanager service")
+	}
+
+	err = wait.PollImmediateInfinite(
+		time.Second*10,
+		o.destroyCluster,
+	)
+	return nil
+
+}
+
+func (o *ClusterUninstaller) destroyCluster() (bool, error) {
+	destroyFuncs := []struct {
+		name    string
+		destroy func() error
+	}{
+		{name: "Compute instances", destroy: o.destroyComputeInstances},
+		{name: "Disks", destroy: o.destroyDisks},
+		{name: "Service accounts", destroy: o.destroyServiceAccounts},
+		{name: "Policy bindings", destroy: o.destroyIAMPolicyBindings},
+		{name: "Images", destroy: o.destroyImages},
+		{name: "DNS", destroy: o.destroyDNS},
+		{name: "Object storage", destroy: o.destroyObjectStorage},
+		{name: "Routes", destroy: o.destroyRoutes},
+		{name: "Firewalls", destroy: o.destroyFirewalls},
+		{name: "Addresses", destroy: o.destroyAddresses},
+		{name: "Target Pools", destroy: o.destroyTargetPools},
+		{name: "Forwarding rules", destroy: o.destroyForwardingRules},
+		{name: "Backend services", destroy: o.destroyBackendServices},
+		{name: "Health checks", destroy: o.destroyHealthChecks},
+		{name: "HTTP Health checks", destroy: o.destroyHTTPHealthChecks},
+		{name: "Cloud controller internal LBs", destroy: o.destroyCloudControllerInternalLBs},
+		{name: "Cloud controller external LBs", destroy: o.destroyCloudControllerExternalLBs},
+		{name: "Cloud routers", destroy: o.destroyRouters},
+		{name: "Subnetworks", destroy: o.destroySubNetworks},
+		{name: "Networks", destroy: o.destroyNetworks},
+	}
+	done := true
+	for _, f := range destroyFuncs {
+		err := f.destroy()
+		if err != nil {
+			o.Logger.Debugf("%s: %v", f.name, err)
+			done = false
+		}
+	}
+	return done, nil
+}
+
+func (o *ClusterUninstaller) isClusterResource(name string) bool {
+	return strings.HasPrefix(name, o.ClusterID+"-")
+}
+
+func (o *ClusterUninstaller) clusterIDFilter() string {
+	return fmt.Sprintf("name eq \"%s-.*\"", o.ClusterID)
+}
+
+func isNoOp(err error) bool {
+	if err == nil {
+		return false
+	}
+	ae, ok := err.(*googleapi.Error)
+	return ok && (ae.Code == http.StatusNotFound || ae.Code == http.StatusNotModified)
+}
+
+func isNotFound(err error) bool {
+	if err == nil {
+		return false
+	}
+	ae, ok := err.(*googleapi.Error)
+	return ok && ae.Code == http.StatusNotFound
+}
+
+// aggregateError is a utility function that takes a slice of errors and an
+// optional pending argument, and returns an error or nil
+func aggregateError(errs []error, pending ...int) error {
+	err := utilerrors.NewAggregate(errs)
+	if err != nil {
+		return err
+	}
+	if len(pending) > 0 && pending[0] > 0 {
+		return errors.Errorf("%d items pending", pending[0])
+	}
+	return nil
+}
+
+func (o *ClusterUninstaller) contextWithTimeout() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(o.Context, defaultTimeout)
+}
+
+// requestIDTracker keeps track of a set of request IDs mapped to a unique resource
+// identifier
+type requestIDTracker struct {
+	requestIDs map[string]string
+}
+
+func newRequestIDTracker() requestIDTracker {
+	return requestIDTracker{
+		requestIDs: map[string]string{},
+	}
+}
+
+// requestID returns a UID for a given item identifier. Unless the ID is reset, the
+// same requestID will be returned every time for a given item.
+func (t requestIDTracker) requestID(identifier ...string) string {
+	key := strings.Join(identifier, "/")
+	id, exists := t.requestIDs[key]
+	if !exists {
+		id = uuid.New()
+		t.requestIDs[key] = id
+	}
+	return id
+}
+
+// resetRequestID resets the request ID used for a particular item. This
+// should be called whenever a request fails, and a brand new request should be
+// sent.
+func (t requestIDTracker) resetRequestID(identifier ...string) {
+	key := strings.Join(identifier, "/")
+	if _, exists := t.requestIDs[key]; exists {
+		delete(t.requestIDs, key)
+	}
+}
+
+// pendingItemTracker tracks a set of pending item names for a given type of resource
+type pendingItemTracker struct {
+	pendingItems map[string]sets.String
+}
+
+func newPendingItemTracker() pendingItemTracker {
+	return pendingItemTracker{
+		pendingItems: map[string]sets.String{},
+	}
+}
+
+// setPendingItems sets the list of items pending deletion for a particular item type.
+// It returns items that were previously pending that are no longer in the list
+// of pending items. These are items that have been deleted.
+func (t pendingItemTracker) setPendingItems(itemType string, items []string) []string {
+	found := sets.NewString(items...)
+	lastFound, exists := t.pendingItems[itemType]
+	if !exists {
+		lastFound = sets.NewString()
+	}
+	deletedItems := lastFound.Difference(found)
+	t.pendingItems[itemType] = found
+	return deletedItems.List()
+}
+
+func isErrorStatus(code int64) bool {
+	return code < 200 || code >= 300
+}
+
+func operationErrorMessage(op *compute.Operation) string {
+	errs := []string{}
+	if op.Error != nil {
+		for _, e := range op.Error.Errors {
+			errs = append(errs, fmt.Sprintf("%s: %s", e.Code, e.Message))
+		}
+	}
+	if len(errs) == 0 {
+		return op.HttpErrorMessage
+	}
+	return strings.Join(errs, ", ")
+}
---
+++ vendor/github.com/openshift/installer/pkg/types/gcp/validation/platform.go
@@ -0,0 +1,59 @@
+package validation
+
+import (
+	"sort"
+
+	"github.com/openshift/installer/pkg/types/gcp"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+var (
+	// Regions is a map of known GCP regions. The key of the map is
+	// the short name of the region. The value of the map is the long
+	// name of the region.
+	Regions = map[string]string{
+		// List from: https://cloud.google.com/compute/docs/regions-zones/
+		"asia-east1":              "Changhua County, Taiwan",
+		"asia-east2":              "Hong Kong",
+		"asia-northeast1":         "Tokyo, Japan",
+		"asia-northeast2":         "Osaka, Japan",
+		"asia-south1":             "Mumbai, India",
+		"asia-southeast1":         "Jurong West, Singapore",
+		"australia-southeast1":    "Sydney, Australia",
+		"europe-north1":           "Hamina, Finland",
+		"europe-west1":            "St. Ghislain, Belgium",
+		"europe-west2":            "London, England, UK",
+		"europe-west3":            "Frankfurt, Germany",
+		"europe-west4":            "Eemshaven, Netherlands",
+		"europe-west6":            "Zürich, Switzerland",
+		"northamerica-northeast1": "Montréal, Québec, Canada",
+		"southamerica-east1":      "São Paulo, Brazil",
+		"us-central1":             "Council Bluffs, Iowa, USA",
+		"us-east1":                "Moncks Corner, South Carolina, USA",
+		"us-east4":                "Ashburn, Northern Virginia, USA",
+		"us-west1":                "The Dalles, Oregon, USA",
+		"us-west2":                "Los Angeles, California, USA",
+	}
+	validRegionValues = func() []string {
+		validValues := make([]string, len(Regions))
+		i := 0
+		for r := range Regions {
+			validValues[i] = r
+			i++
+		}
+		sort.Strings(validValues)
+		return validValues
+	}()
+)
+
+// ValidatePlatform checks that the specified platform is valid.
+func ValidatePlatform(p *gcp.Platform, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	if _, ok := Regions[p.Region]; !ok {
+		allErrs = append(allErrs, field.NotSupported(fldPath.Child("region"), p.Region, validRegionValues))
+	}
+	if p.DefaultMachinePlatform != nil {
+		allErrs = append(allErrs, ValidateMachinePool(p, p.DefaultMachinePlatform, fldPath.Child("defaultMachinePlatform"))...)
+	}
+	return allErrs
+}

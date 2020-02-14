72d9e43734348b5684168bdf5e33438888d8f501
staebler <staebler@redhat.com>
2020-02-14 10:11:06 -0500

    restart pods with injected certs when the certs rotate
    The hiveadmission and hiveapi pods use certs that may change while the pods
    are running. When the certs change, the pods will no longer be able to do
    their work. The pods need to be restarted to pick up the new certs.

--- cmd/hiveadmission/main.go
+++ cmd/hiveadmission/main.go
@@ -5,6 +5,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	hivevalidatingwebhooks "github.com/openshift/hive/pkg/apis/hive/v1/validating-webhooks"
+	"github.com/openshift/hive/pkg/certs"
 )
 
 func main() {
@@ -12,6 +13,11 @@ func main() {
 
 	// TODO: figure out a way to combine logrus and klog logging levels. The team has decided that hardcoding this is ok for now.
 	log.SetLevel(log.InfoLevel)
+
+	// TODO: Would be better to get this from the --tls-cert-file argument. The parsing of the argument is hidden in
+	// the internals of RunAdmissionServer.
+	const certsDir = "/var/serving-cert"
+	certs.TerminateOnCertChanges(certsDir)
 
 	admissionCmd.RunAdmissionServer(
 		&hivevalidatingwebhooks.DNSZoneValidatingAdmissionHook{},

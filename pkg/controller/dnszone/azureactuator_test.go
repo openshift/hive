b8db91586bb7a270b7b7f48ae0db1d6b5742d9be
Waseem Hassan Shahid <waseemhassanshahid@gmail.com>
2020-06-06 00:54:03 +0200

    Add azure actuator test cases

---
+++ pkg/controller/dnszone/azureactuator_test.go
@@ -0,0 +1,92 @@
+package dnszone
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/services/dns/mgmt/2018-05-01/dns"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/to"
+	"github.com/golang/mock/gomock"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+
+	corev1 "k8s.io/api/core/v1"
+
+	hivev1 "github.com/openshift/hive/pkg/apis/hive/v1"
+	"github.com/openshift/hive/pkg/azureclient/mock"
+)
+
+// TestNewAzureActuator tests that a new AzureActuator object can be created.
+func TestNewAzureActuator(t *testing.T) {
+	cases := []struct {
+		name    string
+		dnsZone *hivev1.DNSZone
+		secret  *corev1.Secret
+	}{
+		{
+			name:    "Successfully create new zone",
+			dnsZone: validAzureDNSZone(),
+			secret:  validAzureSecret(),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+			mocks := setupDefaultMocks(t)
+			expectedAzureActuator := &AzureActuator{
+				logger:  log.WithField("controller", ControllerName),
+				dnsZone: tc.dnsZone,
+			}
+
+			// Act
+			zr, err := NewAzureActuator(
+				expectedAzureActuator.logger,
+				tc.secret,
+				tc.dnsZone,
+				fakeAzureClientBuilder(mocks.mockAzureClient),
+			)
+			expectedAzureActuator.azureClient = zr.azureClient // Function pointers can't be compared reliably. Don't compare.
+
+			// Assert
+			assert.Nil(t, err)
+			assert.NotNil(t, zr.azureClient)
+			assert.Equal(t, expectedAzureActuator, zr)
+		})
+	}
+}
+
+func mockAzureZoneExists(expect *mock.MockClientMockRecorder) {
+	expect.GetZone(gomock.Any(), gomock.Any(), gomock.Any()).Return(dns.Zone{
+		Name: to.StringPtr("blah.example.com"),
+		ZoneProperties: &dns.ZoneProperties{
+			NameServers: to.StringSlicePtr([]string{"ns1.example.com", "ns2.example.com"}),
+		},
+	}, nil).Times(1)
+}
+
+func mockAzureZoneDoesntExist(expect *mock.MockClientMockRecorder) {
+	expect.GetZone(gomock.Any(), gomock.Any(), gomock.Any()).
+		Return(dns.Zone{
+			Response: autorest.Response{
+				Response: &http.Response{
+					StatusCode: http.StatusNotFound,
+				},
+			},
+		}, errors.New("Not found")).Times(1)
+}
+
+func mockCreateAzureZone(expect *mock.MockClientMockRecorder) {
+	expect.CreateOrUpdateZone(gomock.Any(), gomock.Any(), gomock.Any()).Return(dns.Zone{
+		Name: to.StringPtr("blah.example.com"),
+		ZoneProperties: &dns.ZoneProperties{
+			NameServers: to.StringSlicePtr([]string{"ns1.example.com", "ns2.example.com"}),
+		},
+	}, nil).Times(1)
+}
+
+func mockDeleteAzureZone(expect *mock.MockClientMockRecorder) {
+	expect.DeleteZone(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil).Times(1)
+}

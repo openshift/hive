275a46a4fa2b79a51942addf89056b96e68f3d7e
staebler <staebler@redhat.com>
2020-09-02 13:23:09 -0400

    remove residual v1alpha1 migration scripts
    Since the v1alpha1 aggregated API server can no longer be run,
    the migration scripts are no longer useful.

--- hack/v1migration/deploy_apiserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-oc patch hiveconfig hive --type='merge' -p $'spec:\n hiveAPIEnabled: true'
--- hack/v1migration/verify_apiserver.sh
+++
@@ -1,7 +0,0 @@
-verify_apiserver() {
-  if [[ $(oc get apiservice v1alpha1.hive.openshift.io -o json | jq '.status.conditions[]? | select(.type=="Available") | .status=="True"') != "true" ]]
-  then
-    echo "Hive aggregated API server is not available"
-    exit 1
-  fi
-}

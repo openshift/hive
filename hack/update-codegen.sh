3d01091643768479f15d751329a3c2c7873e6edd
Abhinav Dahiya <abhinavdtu2012@gmail.com>
2021-02-18 13:07:01 -0800

    update build tools for apis submodule

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -7,7 +7,7 @@ verify="${VERIFY:-}"
 
 GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "all" \
   github.com/openshift/hive/pkg/client \
-  github.com/openshift/hive/pkg/apis \
+  github.com/openshift/hive/apis \
   "hive:v1 hiveinternal:v1alpha1" \
   --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt \
   ${verify}
@@ -15,7 +15,7 @@ GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "all" \
 # Generate deepcopy for platform-specific types.
 GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
   github.com/openshift/hive/pkg/client \
-  github.com/openshift/hive/pkg/apis \
+  github.com/openshift/hive/apis \
   "hive:v1/agent hive:v1/aws hive:v1/azure hive:v1/baremetal hive:v1/gcp hive:v1/openstack hive:v1/ovirt hive:v1/vsphere" \
   --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt \
   ${verify}

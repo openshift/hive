28fe8a1345b4d67d48840f842fc63fc339cfb64f
Devan Goodwin <dgoodwin@redhat.com>
2021-03-03 16:16:14 -0400

    Drop Agent VIPDHCPMode feature, not yet production ready.

--- vendor/github.com/openshift/hive/apis/hive/v1/agent/platform.go
+++ vendor/github.com/openshift/hive/apis/hive/v1/agent/platform.go
@@ -5,6 +5,7 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 // BareMetalPlatform defines agent based install configuration specific to bare metal clusters.
 // Can only be used with spec.installStrategy.agent.
 type BareMetalPlatform struct {
+
 	// APIVIP is the virtual IP used to reach the OpenShift cluster's API.
 	APIVIP string `json:"apiVIP"`
 
@@ -15,20 +16,7 @@ type BareMetalPlatform struct {
 	// IngressVIP is the virtual IP used for cluster ingress traffic.
 	IngressVIP string `json:"ingressVIP"`
 
-	// VIPDHCPAllocation indicates if virtual IP DHCP allocation mode is enabled.
-	// +optional
-	VIPDHCPAllocation VIPDHCPAllocationType `json:"vipDHCPAllocation"`
-
 	// AgentSelector is a label selector used for associating relevant custom resources with this cluster.
 	// (Agent, BareMetalHost, etc)
 	AgentSelector metav1.LabelSelector `json:"agentSelector"`
 }
-
-// VIPDHCPAllocationType is a valid value for bareMetalPlatform.vipDHCPAllocation.
-// +kubebuilder:validation:Enum="";"Enabled"
-type VIPDHCPAllocationType string
-
-const (
-	VIPDHCPAllocationDisabled VIPDHCPAllocationType = ""
-	VIPDHCPAllocationEnabled  VIPDHCPAllocationType = "Enabled"
-)

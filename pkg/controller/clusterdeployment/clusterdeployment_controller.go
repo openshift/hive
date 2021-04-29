1b78f293e85183be221aefe2342e3f9649d66ca4
Devan Goodwin <dgoodwin@redhat.com>
2021-04-29 14:53:56 -0300

    Remove unnecessary ClusterInstallRef check.

--- pkg/controller/clusterdeployment/clusterdeployment_controller.go
+++ pkg/controller/clusterdeployment/clusterdeployment_controller.go
@@ -624,14 +624,6 @@ func (r *ReconcileClusterDeployment) reconcile(request reconcile.Request, cd *hi
 }
 
 func (r *ReconcileClusterDeployment) reconcileInstallingClusterProvision(cd *hivev1.ClusterDeployment, releaseImage string, logger log.FieldLogger) (reconcile.Result, error) {
-	// Return early and stop processing if ClusterInstallRef is in play. The controllers that
-	// handle this portion of the API currently live in the assisted service repo, rather than hive.
-	// This will hopefully change in the future.
-	if cd.Spec.ClusterInstallRef != nil {
-		logger.Info("ClusterInstallRef specified, skipping creation of ClusterProvision")
-		return reconcile.Result{}, nil
-	}
-
 	if cd.Status.ProvisionRef == nil {
 		return r.startNewProvision(cd, releaseImage, logger)
 	}

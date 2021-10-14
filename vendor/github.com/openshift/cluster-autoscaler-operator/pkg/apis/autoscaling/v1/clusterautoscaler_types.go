e252965a78379a64ddc879b4319b80dfb787a178
Andrew Butcher <abutcher@redhat.com>
2021-10-14 14:56:14 -0400

    Bump controller-runtime to v0.10.2

--- vendor/github.com/openshift/cluster-autoscaler-operator/pkg/apis/autoscaling/v1/clusterautoscaler_types.go
+++ vendor/github.com/openshift/cluster-autoscaler-operator/pkg/apis/autoscaling/v1/clusterautoscaler_types.go
@@ -10,10 +10,36 @@ func init() {
 
 // ClusterAutoscalerSpec defines the desired state of ClusterAutoscaler
 type ClusterAutoscalerSpec struct {
-	ResourceLimits       *ResourceLimits  `json:"resourceLimits,omitempty"`
-	ScaleDown            *ScaleDownConfig `json:"scaleDown,omitempty"`
-	MaxPodGracePeriod    *int32           `json:"maxPodGracePeriod,omitempty"`
-	PodPriorityThreshold *int32           `json:"podPriorityThreshold,omitempty"`
+	// Constraints of autoscaling resources
+	ResourceLimits *ResourceLimits `json:"resourceLimits,omitempty"`
+
+	// Configuration of scale down operation
+	ScaleDown *ScaleDownConfig `json:"scaleDown,omitempty"`
+
+	// Gives pods graceful termination time before scaling down
+	MaxPodGracePeriod *int32 `json:"maxPodGracePeriod,omitempty"`
+
+	// Maximum time CA waits for node to be provisioned
+	// +kubebuilder:validation:Pattern=^([0-9]+(\.[0-9]+)?(ns|us|µs|ms|s|m|h))+$
+	MaxNodeProvisionTime string `json:"maxNodeProvisionTime,omitempty"`
+
+	// To allow users to schedule "best-effort" pods, which shouldn't trigger
+	// Cluster Autoscaler actions, but only run when there are spare resources available,
+	// More info: https://github.com/kubernetes/autoscaler/blob/master/cluster-autoscaler/FAQ.md#how-does-cluster-autoscaler-work-with-pod-priority-and-preemption
+	PodPriorityThreshold *int32 `json:"podPriorityThreshold,omitempty"`
+
+	// BalanceSimilarNodeGroups enables/disables the
+	// `--balance-similar-node-groups` cluster-autocaler feature.
+	// This feature will automatically identify node groups with
+	// the same instance type and the same set of labels and try
+	// to keep the respective sizes of those node groups balanced.
+	BalanceSimilarNodeGroups *bool `json:"balanceSimilarNodeGroups,omitempty"`
+
+	// Enables/Disables `--ignore-daemonsets-utilization` CA feature flag. Should CA ignore DaemonSet pods when calculating resource utilization for scaling down. false by default
+	IgnoreDaemonsetsUtilization *bool `json:"ignoreDaemonsetsUtilization,omitempty"`
+
+	// Enables/Disables `--skip-nodes-with-local-storage` CA feature flag. If true cluster autoscaler will never delete nodes with pods with local storage, e.g. EmptyDir or HostPath. true by default at autoscaler
+	SkipNodesWithLocalStorage *bool `json:"skipNodesWithLocalStorage,omitempty"`
 }
 
 // ClusterAutoscalerStatus defines the observed state of ClusterAutoscaler
@@ -25,12 +51,17 @@ type ClusterAutoscalerStatus struct {
 
 // ClusterAutoscaler is the Schema for the clusterautoscalers API
 // +k8s:openapi-gen=true
+// +kubebuilder:resource:path=clusterautoscalers,shortName=ca,scope=Cluster
 // +kubebuilder:subresource:status
+// +genclient:nonNamespaced
 type ClusterAutoscaler struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   ClusterAutoscalerSpec   `json:"spec,omitempty"`
+	// Desired state of ClusterAutoscaler resource
+	Spec ClusterAutoscalerSpec `json:"spec,omitempty"`
+
+	// Most recently observed status of ClusterAutoscaler resource
 	Status ClusterAutoscalerStatus `json:"status,omitempty"`
 }
 
@@ -44,11 +75,22 @@ type ClusterAutoscalerList struct {
 }
 
 type ResourceLimits struct {
+	// Maximum number of nodes in all node groups.
+	// Cluster autoscaler will not grow the cluster beyond this number.
 	// +kubebuilder:validation:Minimum=0
-	MaxNodesTotal *int32         `json:"maxNodesTotal,omitempty"`
-	Cores         *ResourceRange `json:"cores,omitempty"`
-	Memory        *ResourceRange `json:"memory,omitempty"`
-	GPUS          []GPULimit     `json:"gpus,omitempty"`
+	MaxNodesTotal *int32 `json:"maxNodesTotal,omitempty"`
+
+	// Minimum and maximum number of cores in cluster, in the format <min>:<max>.
+	// Cluster autoscaler will not scale the cluster beyond these numbers.
+	Cores *ResourceRange `json:"cores,omitempty"`
+
+	// Minimum and maximum number of gigabytes of memory in cluster, in the format <min>:<max>.
+	// Cluster autoscaler will not scale the cluster beyond these numbers.
+	Memory *ResourceRange `json:"memory,omitempty"`
+
+	// Minimum and maximum number of different GPUs in cluster, in the format <gpu_type>:<min>:<max>.
+	// Cluster autoscaler will not scale the cluster beyond these numbers. Can be passed multiple times.
+	GPUS []GPULimit `json:"gpus,omitempty"`
 }
 
 type GPULimit struct {
@@ -68,13 +110,22 @@ type ResourceRange struct {
 }
 
 type ScaleDownConfig struct {
+	// Should CA scale down the cluster
 	Enabled bool `json:"enabled"`
+
+	// How long after scale up that scale down evaluation resumes
 	// +kubebuilder:validation:Pattern=([0-9]*(\.[0-9]*)?[a-z]+)+
 	DelayAfterAdd *string `json:"delayAfterAdd,omitempty"`
+
+	// How long after node deletion that scale down evaluation resumes, defaults to scan-interval
 	// +kubebuilder:validation:Pattern=([0-9]*(\.[0-9]*)?[a-z]+)+
 	DelayAfterDelete *string `json:"delayAfterDelete,omitempty"`
+
+	// How long after scale down failure that scale down evaluation resumes
 	// +kubebuilder:validation:Pattern=([0-9]*(\.[0-9]*)?[a-z]+)+
 	DelayAfterFailure *string `json:"delayAfterFailure,omitempty"`
+
+	// How long a node should be unneeded before it is eligible for scale down
 	// +kubebuilder:validation:Pattern=([0-9]*(\.[0-9]*)?[a-z]+)+
 	UnneededTime *string `json:"unneededTime,omitempty"`
 }
--- vendor/github.com/openshift/library-go/pkg/manifest/manifest.go
+++ vendor/github.com/openshift/library-go/pkg/manifest/manifest.go
@@ -15,23 +15,50 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 )
 
+// resourceId uniquely identifies a Kubernetes resource.
+// It is used to identify any duplicate resources within
+// a given set of manifests.
+type resourceId struct {
+	// Group identifies a set of API resources exposed together.
+	// +optional
+	Group string
+	// Kind is the name of a particular object schema.
+	Kind string
+	// Name, sometimes used with the optional Namespace, helps uniquely identify an object.
+	Name string
+	// Namespace helps uniquely identify an object.
+	// +optional
+	Namespace string
+}
+
 // Manifest stores Kubernetes object in Raw from a file.
-// It stores the GroupVersionKind for the manifest.
-// Raw and Obj should always be kept in sync such that
-// each provides the same data but in different formats.
-// To ensure Raw and Obj are always in sync, they should not
-// be set directly but rather only be set by calling
-// either method ManifestsFromFiles or ParseManifests.
+// It stores the id and the GroupVersionKind for
+// the manifest. Raw and Obj should always be kept in sync
+// such that each provides the same data but in different
+// formats. To ensure Raw and Obj are always in sync, they
+// should not be set directly but rather only be set by
+// calling either method ManifestsFromFiles or
+// ParseManifests.
 type Manifest struct {
 	// OriginalFilename is set to the filename this manifest was loaded from.
-	// It is not guaranteed to be set or be unique, but we will set it when
-	// loading from disk to provide better debuggability.
+	// It is not guaranteed to be set or be unique, but will be set when
+	// loading from disk to provide a better debug capability.
 	OriginalFilename string
+
+	id resourceId
 
 	Raw []byte
 	GVK schema.GroupVersionKind
 
 	Obj *unstructured.Unstructured
+}
+
+func (r resourceId) String() string {
+	if len(r.Namespace) == 0 {
+		return fmt.Sprintf("Group: %q Kind: %q Name: %q", r.Group, r.Kind, r.Name)
+	} else {
+		return fmt.Sprintf("Group: %q Kind: %q Namespace: %q Name: %q", r.Group, r.Kind, r.Namespace, r.Name)
+	}
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface for the Manifest
@@ -60,16 +87,25 @@ func (m *Manifest) UnmarshalJSON(in []byte) error {
 	if !ok {
 		return fmt.Errorf("expected manifest to decode into *unstructured.Unstructured, got %T", ud)
 	}
-
 	m.GVK = ud.GroupVersionKind()
 	m.Obj = ud
-	return nil
+	m.id = resourceId{
+		Group:     m.GVK.Group,
+		Kind:      m.GVK.Kind,
+		Namespace: m.Obj.GetNamespace(),
+		Name:      m.Obj.GetName(),
+	}
+	return validateResourceId(m.id)
 }
 
 // ManifestsFromFiles reads files and returns Manifests in the same order.
-// files should be list of absolute paths for the manifests on disk.
+// 'files' should be list of absolute paths for the manifests on disk. An
+// error is returned for each manifest that defines a duplicate resource
+// as compared to other manifests defined within the 'files' list.
+// Duplicate resources have the same group, kind, name, and namespace.
 func ManifestsFromFiles(files []string) ([]Manifest, error) {
 	var manifests []Manifest
+	ids := make(map[resourceId]bool)
 	var errs []error
 	for _, file := range files {
 		file, err := os.Open(file)
@@ -86,6 +122,10 @@ func ManifestsFromFiles(files []string) ([]Manifest, error) {
 		}
 		for _, m := range ms {
 			m.OriginalFilename = filepath.Base(file.Name())
+			err = addIfNotDuplicateResource(m, ids)
+			if err != nil {
+				errs = append(errs, errors.Wrapf(err, "File %s contains", file.Name()))
+			}
 		}
 		manifests = append(manifests, ms...)
 	}
@@ -99,8 +139,10 @@ func ManifestsFromFiles(files []string) ([]Manifest, error) {
 }
 
 // ParseManifests parses a YAML or JSON document that may contain one or more
-// kubernetes resources.
+// kubernetes resources. An error is returned if the input cannot be parsed
+// or contains a duplicate resource.
 func ParseManifests(r io.Reader) ([]Manifest, error) {
+	theseIds := make(map[resourceId]bool)
 	d := yaml.NewYAMLOrJSONDecoder(r, 1024)
 	var manifests []Manifest
 	for {
@@ -115,6 +157,26 @@ func ParseManifests(r io.Reader) ([]Manifest, error) {
 		if len(m.Raw) == 0 || bytes.Equal(m.Raw, []byte("null")) {
 			continue
 		}
+		if err := addIfNotDuplicateResource(m, theseIds); err != nil {
+			return manifests, err
+		}
 		manifests = append(manifests, m)
 	}
+}
+
+// validateResourceId ensures the id contains the required fields per
+// https://kubernetes.io/docs/concepts/overview/working-with-objects/kubernetes-objects/#required-fields.
+func validateResourceId(id resourceId) error {
+	if id.Kind == "" || id.Name == "" {
+		return fmt.Errorf("Resource with fields %s must contain kubernetes required fields kind and name", id)
+	}
+	return nil
+}
+
+func addIfNotDuplicateResource(manifest Manifest, resourceIds map[resourceId]bool) error {
+	if _, ok := resourceIds[manifest.id]; !ok {
+		resourceIds[manifest.id] = true
+		return nil
+	}
+	return fmt.Errorf("duplicate resource: (%s)", manifest.id)
 }

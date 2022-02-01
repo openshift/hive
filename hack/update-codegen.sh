7cbecd9f2ab5a2bfb55ec8d757df7bacea599af9
Eric Fried <efried@redhat.com>
2022-02-01 11:54:54 -0600

    Hack GOPATH for codegen
    Copy/adapt
    [GOPATH ridiculousness](https://github.com/openshift/cloud-credential-operator/blob/aa5510254782d506f59a0578661f22a8d639dc85/hack/update-codegen.sh)
    from CCO to work around codegen bugs that would generate the code under
    github.com/ in the repo root.
    
    Check in the resulting deepcopy deltas we've apparently been missing for
    a while.
    
    HIVE-1723

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
+# set the passed in directory as a usable GOPATH
+# that deepcopy-gen can operate in
+ensure-temp-gopath() {
+	fake_gopath=$1
+
+	# set up symlink pointing to our repo root
+	fake_repopath=$fake_gopath/src/github.com/openshift/hive
+	mkdir -p "$(dirname "${fake_repopath}")"
+	ln -s "$REPO_FULL_PATH" "${fake_repopath}"
+}
+
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+REPO_FULL_PATH=$(realpath ${SCRIPT_ROOT})
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
 
 verify="${VERIFY:-}"
 
-GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "all" \
+valid_gopath=$(realpath $REPO_FULL_PATH/../../../..)
+if [[ "$(realpath ${valid_gopath}/src/github.com/openshift/hive)" == "${REPO_FULL_PATH}" ]]; then
+	temp_gopath=${valid_gopath}
+else
+	TMP_DIR=$(mktemp -d -t hive-codegen.XXXX)
+	function finish {
+		chmod -R +w ${TMP_DIR}
+		# ok b/c we will symlink to the original repo
+		rm -r ${TMP_DIR}
+	}
+	trap finish EXIT
+
+	ensure-temp-gopath ${TMP_DIR}
+
+	temp_gopath=${TMP_DIR}
+fi
+
+GOPATH="${temp_gopath}" GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "all" \
   github.com/openshift/hive/pkg/client \
   github.com/openshift/hive/apis \
   "hive:v1 hiveinternal:v1alpha1" \
@@ -13,7 +42,7 @@ GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "all" \
   ${verify}
 
 # Generate deepcopy for platform-specific types.
-GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
+GOPATH="${temp_gopath}" GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
   github.com/openshift/hive/pkg/client \
   github.com/openshift/hive/apis \
   "hive:v1/agent hive:v1/aws hive:v1/azure hive:v1/baremetal hive:v1/gcp hive:v1/openstack hive:v1/ovirt hive:v1/vsphere hivecontracts:v1alpha1" \

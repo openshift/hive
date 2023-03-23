openshift/hive

commit 0f4b1de78a25a7c1b031089ca3ac59ea320cc144

Move secret/configmap retrieval to hiveutil for prov/deprov
Pods created by hive-controllers require information from secrets and
configmaps (for example, cloud credentials) in order to function.
Previously these were passed into the pods from the hive-controllers side via
environment variables and volumes. In scale mode (see HIVE-1861) we're
going to need that same business logic -- which parts of which objects
are needed in what form -- on the hiveutil side.

In order to avoid duplication of that logic, we're moving it *all* to
the hiveutil side, even for non-scale mode.

The cloud credentials secret and other secrets/configmaps are no longer
passed through via the pod spec. Instead, the hiveutil command itself
uses a local client to load those secrets and set up the same
environment variables and files that were previously passed through on
the pod.

HIVE-2021

contrib/pkg/deprovision/awstagdeprovision.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/openshift/hive/contrib/pkg/utils"
+	awsutils "github.com/openshift/hive/contrib/pkg/utils/aws"
 	"github.com/openshift/installer/pkg/destroy/aws"
 )
 
@@ -59,6 +61,12 @@ func completeAWSUninstaller(o *aws.ClusterUninstaller, logLevel string, args []s
 	if o.Logger, err = utils.NewLogger(logLevel); err != nil {
 		return err
 	}
+
+	client, err := utils.GetClient()
+	if err != nil {
+		return errors.Wrap(err, "failed to get client")
+	}
+	awsutils.ConfigureCreds(client)
 
 	return nil
 }

contrib/pkg/deprovision/alibabacloud.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 
 	"github.com/openshift/hive/contrib/pkg/utils"
+	aliutils "github.com/openshift/hive/contrib/pkg/utils/alibabacloud"
 	"github.com/openshift/installer/pkg/destroy/alibabacloud"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
@@ -55,6 +57,11 @@ func NewDeprovisionAlibabaCloudCommand() *cobra.Command {
 // Complete finishes parsing arguments for the command
 func (o *alibabaCloudDeprovisionOptions) Complete(cmd *cobra.Command, args []string) error {
 	o.infraID = args[0]
+	client, err := utils.GetClient()
+	if err != nil {
+		return errors.Wrap(err, "failed to get client")
+	}
+	aliutils.ConfigureCreds(client)
 	return nil
 }
 

contrib/pkg/deprovision/azure.go
@@ -28,13 +28,13 @@ func NewDeprovisionAzureCommand() *cobra.Command {
 		Short: "Deprovision Azure assets (as created by openshift-installer)",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			if err := validate(); err != nil {
-				log.WithError(err).Fatal("Failed validating Azure credentials")
-			}
 			uninstaller, err := completeAzureUninstaller(opt.logLevel, opt.cloudName, args)
 			if err != nil {
 				log.WithError(err).Error("Cannot complete command")
 				return
+			}
+			if err := validate(); err != nil {
+				log.WithError(err).Fatal("Failed validating Azure credentials")
 			}
 
 			// ClusterQuota stomped in return
@@ -64,6 +64,12 @@ func completeAzureUninstaller(logLevel, cloudName string, args []string) (provid
 	if err != nil {
 		return nil, err
 	}
+
+	client, err := utils.GetClient()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get client")
+	}
+	azureutils.ConfigureCreds(client)
 
 	metadata := &types.ClusterMetadata{
 		InfraID: args[0],

contrib/pkg/deprovision/gcp.go
@@ -52,6 +52,13 @@ func NewDeprovisionGCPCommand() *cobra.Command {
 // Complete finishes parsing arguments for the command
 func (o *gcpOptions) Complete(cmd *cobra.Command, args []string) error {
 	o.infraID = args[0]
+
+	client, err := utils.GetClient()
+	if err != nil {
+		return errors.Wrap(err, "failed to get client")
+	}
+	gcputils.ConfigureCreds(client)
+
 	return nil
 }
 

contrib/pkg/deprovision/ibmcloud.go
@@ -10,6 +10,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/openshift/hive/contrib/pkg/utils"
+	ibmutils "github.com/openshift/hive/contrib/pkg/utils/ibmcloud"
 	"github.com/openshift/hive/pkg/constants"
 	"github.com/openshift/hive/pkg/ibmclient"
 	"github.com/openshift/installer/pkg/destroy/ibmcloud"
@@ -62,6 +63,12 @@ func NewDeprovisionIBMCloudCommand() *cobra.Command {
 // Complete finishes parsing arguments for the command
 func (o *ibmCloudDeprovisionOptions) Complete(cmd *cobra.Command, args []string) error {
 	o.infraID = args[0]
+
+	client, err := utils.GetClient()
+	if err != nil {
+		return errors.Wrap(err, "failed to get client")
+	}
+	ibmutils.ConfigureCreds(client)
 
 	// Create IBMCloud Client
 	ibmCloudAPIKey := os.Getenv(constants.IBMCloudAPIKeyEnvVar)

contrib/pkg/deprovision/openstack.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/openshift/hive/contrib/pkg/utils"
+	openstackutils "github.com/openshift/hive/contrib/pkg/utils/openstack"
 	"github.com/openshift/installer/pkg/destroy/openstack"
 	"github.com/openshift/installer/pkg/types"
 	typesopenstack "github.com/openshift/installer/pkg/types/openstack"
@@ -48,6 +50,13 @@ func NewDeprovisionOpenStackCommand() *cobra.Command {
 // Complete finishes parsing arguments for the command
 func (o *openStackOptions) Complete(cmd *cobra.Command, args []string) error {
 	o.infraID = args[0]
+
+	client, err := utils.GetClient()
+	if err != nil {
+		return errors.Wrap(err, "failed to get client")
+	}
+	openstackutils.ConfigureCreds(client)
+
 	return nil
 }
 

contrib/pkg/deprovision/ovirt.go
@@ -1,12 +1,12 @@
 package deprovision
 
 import (
-	"errors"
-
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/openshift/hive/contrib/pkg/utils"
+	ovirtutils "github.com/openshift/hive/contrib/pkg/utils/ovirt"
 	"github.com/openshift/installer/pkg/destroy/ovirt"
 	"github.com/openshift/installer/pkg/types"
 	typesovirt "github.com/openshift/installer/pkg/types/ovirt"
@@ -47,6 +47,13 @@ func NewDeprovisionOvirtCommand() *cobra.Command {
 // Complete finishes parsing arguments for the command
 func (o *oVirtOptions) Complete(cmd *cobra.Command, args []string) error {
 	o.infraID = args[0]
+
+	client, err := utils.GetClient()
+	if err != nil {
+		return errors.Wrap(err, "failed to get client")
+	}
+	ovirtutils.ConfigureCreds(client)
+
 	return nil
 }
 

contrib/pkg/deprovision/vsphere.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
@@ -12,6 +13,7 @@ import (
 	typesvsphere "github.com/openshift/installer/pkg/types/vsphere"
 
 	"github.com/openshift/hive/contrib/pkg/utils"
+	vsphereutils "github.com/openshift/hive/contrib/pkg/utils/vsphere"
 	"github.com/openshift/hive/pkg/constants"
 )
 
@@ -52,6 +54,13 @@ func NewDeprovisionvSphereCommand() *cobra.Command {
 // Complete finishes parsing arguments for the command
 func (o *vSphereOptions) Complete(cmd *cobra.Command, args []string) error {
 	o.infraID = args[0]
+
+	client, err := utils.GetClient()
+	if err != nil {
+		return errors.Wrap(err, "failed to get client")
+	}
+	vsphereutils.ConfigureCreds(client)
+
 	return nil
 }
 

commit 11e74864b1daa9a45b5c852e34eee372e672e001

hiveutil aws-tag-deprovision: don't require a cluster
Changes made via #1874 / 0f4b1de caused hiveutil deprovisioning commands
to try to load up a cloud credentials secret upon startup. When run in
standalone mode, this doesn't work (unless you have configured some very
specific environment variables) BUT the deprovisioner is able to work
without it as long as your cloud creds are in one of the usual expected
places (e.g. credentials file, env vars). This is in fact how those
deprovisioners have always worked when invoked standalone.

However, prior to this commit, if we're entirely unable to contact the
cluster API, we would abort the command. With this commit, we instead
print a warning and proceed.

Note that we're only doing this for aws-tag-deprovision at this time, as
it's the only deprovisioner used in standalone mode with any regularity
(to our knowledge).

HIVE-2142

contrib/pkg/deprovision/awstagdeprovision.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
@@ -64,7 +63,9 @@ func completeAWSUninstaller(o *aws.ClusterUninstaller, logLevel string, args []s
 
 	client, err := utils.GetClient()
 	if err != nil {
-		return errors.Wrap(err, "failed to get client")
+		o.Logger.Warnf("Failed to get client: %v\n"+
+			"This is expected when in standalone mode. "+
+			"We expect to find your AWS credentials in one of the usual places.", err)
 	}
 	awsutils.ConfigureCreds(client)
 
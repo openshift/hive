8a156c74c64169aab943d9fc83a8e98423139f70
Mark Old <mold@redhat.com>
2024-03-20 13:10:46 -0700

    Reuse restConfigClientGetter when creating cmdutil.Factory
    Instead of creating and throwing away a RESTMapper every reconcile loop,
    store and reuse it for future reconcile loops. See comments in code for
    further explanation.

--- pkg/resource/restconfig_factory.go
+++ pkg/resource/restconfig_factory.go
@@ -2,13 +2,35 @@ package resource
 
 import (
 	controllerutils "github.com/openshift/hive/pkg/controller/utils"
+	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/restmapper"
 	"k8s.io/client-go/tools/clientcmd"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"sync"
 )
+
+// HIVE-2399: In the cluster sync controller, getRESTConfigFactory is called once per
+// reconcile loop. Previously, a restConfigClientGetter was created for every reconcile,
+// used to apply (or rather, attempt to apply) various synced objects, and then thrown
+// away at the end of the reconcile pass.
+//
+// The restConfigClientGetter (or more accurately, the  cmdutil.Factory that wraps it) is
+// used by the cluster sync controller (via this helper) to apply the various objects to
+// the target cluster during the syncing process. During the application process (see apply.go),
+// the apply command is set up (aptly named setupApplyCommand()) using the factory returned
+// by the getRESTConfigFactory() below. Within this setup, the ToRESTMapper() function of the
+// restConfigClientGetter is invoked. While the mapper returned is a deferred variant,
+// eventually the inner NewDiscoveryRESTMapper() function is invoked, which is very expensive
+// and allocates a huge amount of memory over time if repeatedly called in a hot loop. A graphic
+// demonstrating these allocations is attached the the HIVE-2399 card.
+//
+// The solution is twofold:
+// 1) store the deferred mapper on the restConfigClientGetter so that it can be reused
+// 2) cache the restConfigClientGetter for each rest config and reuse it for future reconciles
+var restConfigClientGetterCache sync.Map
 
 func (r *helper) getRESTConfigFactory(namespace string) (cmdutil.Factory, error) {
 	if r.metricsEnabled {
@@ -17,8 +39,21 @@ func (r *helper) getRESTConfigFactory(namespace string) (cmdutil.Factory, error)
 		controllerutils.AddControllerMetricsTransportWrapper(cfg, r.controllerName, false)
 		r.restConfig = cfg
 	}
-	r.logger.WithField("cache-dir", r.cacheDir).Debug("creating cmdutil.Factory from REST client config and cache directory")
-	f := cmdutil.NewFactory(&restConfigClientGetter{restConfig: r.restConfig, cacheDir: r.cacheDir, namespace: namespace})
+
+	// HIVE-2399: reuse the same restConfigClientGetter for each restConfig
+	restClientGetter, loaded := restConfigClientGetterCache.LoadOrStore(r.restConfig, &restConfigClientGetter{
+		restConfig: r.restConfig,
+		cacheDir:   r.cacheDir,
+		namespace:  namespace,
+		logger:     r.logger,
+	})
+
+	r.logger.
+		WithField("cache-dir", r.cacheDir).
+		WithField("reused-getter", !loaded).
+		Debug("creating cmdutil.Factory from REST client config and cache directory")
+
+	f := cmdutil.NewFactory(restClientGetter.(*restConfigClientGetter))
 	return f, nil
 }
 
@@ -26,6 +61,12 @@ type restConfigClientGetter struct {
 	restConfig *rest.Config
 	cacheDir   string
 	namespace  string
+	logger     log.FieldLogger
+
+	// HIVE-2399: since we are reusing the restConfigClientGetter, it may used concurrently
+	mu              sync.Mutex
+	mapper          *restmapper.DeferredDiscoveryRESTMapper
+	discoveryClient discovery.CachedDiscoveryInterface
 }
 
 // ToRESTConfig returns restconfig
@@ -41,16 +82,26 @@ func (r *restConfigClientGetter) ToDiscoveryClient() (discovery.CachedDiscoveryI
 
 // ToRESTMapper returns a restmapper
 func (r *restConfigClientGetter) ToRESTMapper() (meta.RESTMapper, error) {
-	discoveryClient, err := r.ToDiscoveryClient()
-	if err != nil {
-		return nil, err
+	// HIVE-2399: store the mapper and its discovery client for future use
+	r.mu.Lock()
+	if r.mapper == nil {
+		r.logger.Debugf("creating discovery client and mapper for ToRESTMapper()")
+		discoveryClient, err := r.ToDiscoveryClient()
+		if err != nil {
+			return nil, err
+		}
+		r.mapper = restmapper.NewDeferredDiscoveryRESTMapper(discoveryClient)
+		r.discoveryClient = discoveryClient
+	} else {
+		r.logger.Debugf("reusing discovery client and mapper for ToRESTMapper()")
 	}
+	r.mu.Unlock()
 
-	mapper := restmapper.NewDeferredDiscoveryRESTMapper(discoveryClient)
 	expander := restmapper.NewShortcutExpander(
-		mapper, discoveryClient,
-		// TODO: Plumb logger through restconfigClientGetter and log warnings here
-		func(string) {})
+		r.mapper, r.discoveryClient,
+		func(warning string) {
+			r.logger.Warnln(warning)
+		})
 	return expander, nil
 }
 

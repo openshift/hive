fdc3089c302ad72a4429f629ee01c81ba8103bc1
Leah Leshchinsky <lleshchi@redhat.com>
2024-06-28 15:18:16 -0400

    Rewrite hack/update-codegen.sh
    Update hack/update-codegen.sh to use
    k8s.io/code-generator/kube_codegen.sh, as generate-groups.sh has been
    removed.
    
    Signed-off-by: Leah Leshchinsky <lleshchi@redhat.com>

--- apis/hive/v1/ovirt/doc.go
+++ apis/hive/v1/ovirt/doc.go
@@ -1,4 +1,4 @@
 // Package ovirt contains ovirt-specific structures for
 // installer configuration and management.
-// +k8s:deepcopy-gen=package,register
+// +k8s:deepcopy-gen=package
 package ovirt
--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,36 +1,29 @@
 #!/bin/bash -xe
 
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
+HIVE_ROOT=$(dirname ${BASH_SOURCE})/..
+pushd $HIVE_ROOT
+HIVE_ROOT=$(pwd)
+popd
 
-# use gsed for MAC env
-SED_CMD=sed
-if [[ `uname` == 'Darwin' ]]; then
-  SED_CMD=gsed
-fi
-
-# HACK: For some reason this script is not executable.
-${SED_CMD} -i 's,^exec \(.*/generate-internal-groups.sh\),bash \1,g' ${CODEGEN_PKG}/generate-groups.sh
-# ...but we have to put it back, or `verify` will puke.
-trap "git checkout ${CODEGEN_PKG}/generate-groups.sh" EXIT
-
-cd "${SCRIPT_ROOT}"
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${HIVE_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
 
 ###
 # NOTE: Keep Makefile's `verify-codegen` in sync with the paths in these commands (the second and third arg)
 ###
 
-GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
-  github.com/openshift/hive/pkg/client \
-  github.com/openshift/hive/apis \
-  "hive:v1 hiveinternal:v1alpha1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt \
-  --trim-path-prefix github.com/openshift/hive
+source "${CODEGEN_PKG}/kube_codegen.sh"
 
-# Generate deepcopy for platform-specific types.
-GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
-  github.com/openshift/hive/pkg/client \
-  github.com/openshift/hive/apis \
-  "hive:v1/agent hive:v1/aws hive:v1/azure hive:v1/baremetal hive:v1/gcp hive:v1/metricsconfig hive:v1/none hive:v1/openstack hive:v1/ovirt hive:v1/vsphere hive:v1/ibmcloud hivecontracts:v1alpha1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt \
-  --trim-path-prefix github.com/openshift/hive
+pushd "${HIVE_ROOT}/apis"
+kube::codegen::gen_helpers \
+  --boilerplate "${HIVE_ROOT}/hack/boilerplate.go.txt" \
+  "${HIVE_ROOT}/apis"
+# the gen_helpers function clobbers the vendor folder, so we have to restore it
+git restore "${HIVE_ROOT}/apis/vendor"
+popd
+
+# NOTE (28 June 2024): This is currently a no-op, as there is nothing to generate in pkg/client
+pushd "${HIVE_ROOT}"
+kube::codegen::gen_helpers \
+  --boilerplate "${HIVE_ROOT}/hack/boilerplate.go.txt" \
+  "${HIVE_ROOT}/pkg/client"
+popd

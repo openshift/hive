b9d2ed9acec3d266c16d6825f4c651fa6dc21ef2
Eric Fried <efried@redhat.com>
2024-07-17 17:14:21 -0500

    Forbid kubeconfig exec
    Users with write access to the admin kubeconfig Secret for a given
    ClusterDeployment should not be able to execute arbitrary code in the
    privileged environment in which we run the controllers that use those
    Secrets. Funnel all code paths that load such Secrets through a
    validator to ensure that the AuthInfos[].Exec path is not used.
    
    HIVE-2485
    
    (cherry picked from commit df1ea18fbe5e57fca5f98383da0feb606b9afdc1)

--- pkg/controller/utils/secrets.go
+++ pkg/controller/utils/secrets.go
@@ -2,12 +2,16 @@ package utils
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/openshift/hive/pkg/constants"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -18,11 +22,59 @@ func LoadSecretData(c client.Client, secretName, namespace, dataKey string) (str
 	if err != nil {
 		return "", err
 	}
-	retStr, ok := s.Data[dataKey]
+	retBytes, ok := s.Data[dataKey]
 	if !ok {
 		return "", fmt.Errorf("secret %s did not contain key %s", secretName, dataKey)
 	}
-	return string(retStr), nil
+	switch dataKey {
+	case constants.KubeconfigSecretKey, constants.RawKubeconfigSecretKey:
+		if _, err := validateKubeconfig(retBytes); err != nil {
+			return "", err
+		}
+	}
+	return string(retBytes), nil
+}
+
+// RestConfigFromSecret accepts a Secret containing `kubeconfig` and/or `raw-kubeconfig` keys
+// and returns a rest.Config loaded therefrom.
+// If tryRaw is true, we will look for `raw-kubeconfig` first and use it if present, falling
+// back to `kubeconfig` otherwise.
+// The error return is non-nil if:
+// - The Secret's Data does not contain the [raw-]kubeconfig key(s)
+// - The kubeconfig data cannot be Load()ed
+// - The kubeconfig is insecure
+func RestConfigFromSecret(kubeconfigSecret *corev1.Secret, tryRaw bool) (*rest.Config, error) {
+	var kubeconfigData []byte
+	if tryRaw {
+		kubeconfigData = kubeconfigSecret.Data[constants.RawKubeconfigSecretKey]
+	}
+	if len(kubeconfigData) == 0 {
+		kubeconfigData = kubeconfigSecret.Data[constants.KubeconfigSecretKey]
+	}
+	if len(kubeconfigData) == 0 {
+		return nil, errors.New("kubeconfig secret does not contain necessary data")
+	}
+	config, err := validateKubeconfig(kubeconfigData)
+	if err != nil {
+		return nil, err
+	}
+	kubeConfig := clientcmd.NewDefaultClientConfig(*config, &clientcmd.ConfigOverrides{})
+	return kubeConfig.ClientConfig()
+}
+
+// validateKubeconfig ensures the kubeconfig represented by kc does not use insecure paths.
+// HIVE-2485
+func validateKubeconfig(kc []byte) (*api.Config, error) {
+	config, err := clientcmd.Load(kc)
+	if err != nil {
+		return nil, err
+	}
+	for k, ai := range config.AuthInfos {
+		if ai.Exec != nil {
+			return nil, fmt.Errorf("insecure exec in AuthInfos[%s]", k)
+		}
+	}
+	return config, nil
 }
 
 // Generate the name of the Secret containing AWS Service Provider configuration. The

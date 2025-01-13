afcf08f0576a18c88a54f463d580905a9ec5a1a0
Mark Old <mold@redhat.com>
2025-01-13 15:05:42 -0800

    HIVE-2391: vSphere zonal support

--- vendor/github.com/openshift/hive/apis/hive/v1/vsphere/platform.go
+++ vendor/github.com/openshift/hive/apis/hive/v1/vsphere/platform.go
@@ -1,13 +1,21 @@
 package vsphere
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/go-logr/logr"
+	configv1 "github.com/openshift/api/config/v1"
 	corev1 "k8s.io/api/core/v1"
 )
 
 // Platform stores any global configuration used for vSphere platforms.
 type Platform struct {
 	// VCenter is the domain name or IP address of the vCenter.
-	VCenter string `json:"vCenter"`
+	// Deprecated: Please use Platform.VSphere instead
+	// See also: Platform.ConvertDeprecatedFields
+	// +optional
+	VCenter string `json:"vCenter,omitempty"`
 
 	// CredentialsSecretRef refers to a secret that contains the vSphere account access
 	// credentials: GOVC_USERNAME, GOVC_PASSWORD fields.
@@ -18,18 +26,95 @@ type Platform struct {
 	CertificatesSecretRef corev1.LocalObjectReference `json:"certificatesSecretRef"`
 
 	// Datacenter is the name of the datacenter to use in the vCenter.
-	Datacenter string `json:"datacenter"`
+	// Deprecated: Please use Platform.VSphere instead
+	// See also: Platform.ConvertDeprecatedFields
+	// +optional
+	Datacenter string `json:"datacenter,omitempty"`
 
 	// DefaultDatastore is the default datastore to use for provisioning volumes.
-	DefaultDatastore string `json:"defaultDatastore"`
+	// Deprecated: Please use Platform.VSphere instead
+	// See also: Platform.ConvertDeprecatedFields
+	// +optional
+	DefaultDatastore string `json:"defaultDatastore,omitempty"`
 
 	// Folder is the name of the folder that will be used and/or created for
 	// virtual machines.
+	// Deprecated: Please use Platform.VSphere instead
+	// See also: Platform.ConvertDeprecatedFields
+	// +optional
 	Folder string `json:"folder,omitempty"`
 
 	// Cluster is the name of the cluster virtual machines will be cloned into.
+	// Deprecated: Please use Platform.VSphere instead
+	// See also: Platform.ConvertDeprecatedFields
+	// +optional
 	Cluster string `json:"cluster,omitempty"`
 
 	// Network specifies the name of the network to be used by the cluster.
+	// Deprecated: Please use Platform.VSphere instead
+	// See also: Platform.ConvertDeprecatedFields
+	// +optional
 	Network string `json:"network,omitempty"`
+
+	// VSphere is the full spec of the vSphere platform.
+	VSphere *configv1.VSpherePlatformSpec `json:"vSphere,omitempty"`
+}
+
+func (p *Platform) ConvertDeprecatedFields(logger logr.Logger) {
+	if p.VSphere != nil {
+		return
+	}
+
+	p.VSphere = &configv1.VSpherePlatformSpec{
+		VCenters: []configv1.VSpherePlatformVCenterSpec{
+			{
+				Server:      p.VCenter,
+				Port:        443,
+				Datacenters: []string{p.Datacenter},
+			},
+		},
+		FailureDomains: []configv1.VSpherePlatformFailureDomainSpec{
+			{
+				// names from https://github.com/openshift/installer/blob/f7731922a0f17a8339a3e837f72898ac77643611/pkg/types/vsphere/conversion/installconfig.go#L58-L61
+				Name:   "generated-failure-domain",
+				Region: "generated-region",
+				Zone:   "generated-zone",
+				Server: p.VCenter,
+				Topology: configv1.VSpherePlatformTopology{
+					Datacenter:     p.Datacenter,
+					Datastore:      setDatastorePath(p.DefaultDatastore, p.Datacenter, logger),
+					Folder:         setFolderPath(p.Folder, p.Datacenter, logger),
+					ComputeCluster: setComputeClusterPath(p.Cluster, p.Datacenter, logger),
+					Networks:       []string{p.Network},
+				},
+			},
+		},
+	}
+
+}
+
+// Copied (and slightly modified) from https://github.com/openshift/installer/blob/f7731922a0f17a8339a3e837f72898ac77643611/pkg/types/vsphere/conversion/installconfig.go#L75-L97
+
+func setComputeClusterPath(cluster, datacenter string, logger logr.Logger) string {
+	if cluster != "" && !strings.HasPrefix(cluster, "/") {
+		logger.V(1).Info(fmt.Sprintf("computeCluster as a non-path is now depreciated please use the form: /%s/host/%s", datacenter, cluster))
+		return fmt.Sprintf("/%s/host/%s", datacenter, cluster)
+	}
+	return cluster
+}
+
+func setDatastorePath(datastore, datacenter string, logger logr.Logger) string {
+	if datastore != "" && !strings.HasPrefix(datastore, "/") {
+		logger.V(1).Info(fmt.Sprintf("datastore as a non-path is now depreciated please use the form: /%s/datastore/%s", datacenter, datastore))
+		return fmt.Sprintf("/%s/datastore/%s", datacenter, datastore)
+	}
+	return datastore
+}
+
+func setFolderPath(folder, datacenter string, logger logr.Logger) string {
+	if folder != "" && !strings.HasPrefix(folder, "/") {
+		logger.V(1).Info(fmt.Sprintf("folder as a non-path is now depreciated please use the form: /%s/vm/%s", datacenter, folder))
+		return fmt.Sprintf("/%s/vm/%s", datacenter, folder)
+	}
+	return folder
 }

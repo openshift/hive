19da94eb41a01cfd059cfd375fac10032cc1deb8
Ankita Thomas <ankithom@redhat.com>
2026-08-03 11:54:54 -0400

    Consolidate presubmits, remove MCE 2.7 EOL postsubmit, add arm presubmit, update copyconfig.sh
    Signed-off-by: Ankita Thomas <ankithom@redhat.com>

--- .tekton/copyconfig.sh
+++ .tekton/copyconfig.sh
@@ -13,15 +13,9 @@ if [ "$1" == "26" ]; then
 fi
 
 BASE_PUSH_FILE="hive-mce-$1-push.yaml"
-BASE_PULL_FILE="hive-mce-$1-pull-request.yaml"
 
 if [ ! -f "$BASE_PUSH_FILE" ]; then
     echo "$BASE_PUSH_FILE does not exist"
-    exit 1
-fi
-
-if [ ! -f "$BASE_PULL_FILE" ]; then
-    echo "$BASE_PULL_FILE does not exist"
     exit 1
 fi
 
@@ -31,21 +25,12 @@ if [ -z "$2" ]; then
 fi
 
 NEW_PUSH_FILE="hive-mce-$2-push.yaml"
-NEW_PULL_FILE="hive-mce-$2-pull-request.yaml"
-
 
 if [ -f "$NEW_PUSH_FILE" ]; then
     echo "$NEW_PUSH_FILE already exists"
     exit 1
 fi
 
+cp "$BASE_PUSH_FILE" "$NEW_PUSH_FILE"
 
-if [ -f "$NEW_PULL_FILE" ]; then
-    echo "$NEW_PULL_FILE already exists"
-    exit 1
-fi
-
-cp $BASE_PUSH_FILE $NEW_PUSH_FILE
-cp $BASE_PULL_FILE $NEW_PULL_FILE
-
-sed -i "s/mce-$1/mce-$2/g" $NEW_PUSH_FILE $NEW_PULL_FILE
+sed -i "s/mce-$1/mce-$2/g" "$NEW_PUSH_FILE"
